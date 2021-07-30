f795db8c12e49ff12fc6503d703cd2120d39714d
imadetassaoui <imadetassaoui@gmail.com>
2021-07-30 16:28:29 +0100

    update addTask

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 //Variables
 
 const add = document.querySelector("#add");
-
+//const uncheck=document.querySelector("#checked")
 let tasks = document.querySelector("#tasks");
 let task = document.querySelector(".task");
+//let box = document.getElementById("taskk");
+
 
 const addTask = () => {
     let newTask = document.getElementById("newTask").value;
     let li = document.createElement("li");
     let input = document.createElement("input");
-    li.setAttribute("type", "checkbox");
-    li.appendChild(document.createTextNode(newTask));
-    li.appendChild(document.createTextNode(input));
+    let label=document.createElement("label");
+    input.setAttribute("type","checkbox");
+    //input.setAttribute("id","test");
+    //li.setAttribute("id","taskk")
+    if(newTask!=""){
+    label.appendChild(document.createTextNode(newTask));
     tasks.appendChild(li);
+    li.appendChild(input);
+    li.appendChild(label);
+    }
+}
+const valid = () =>{
+alert("test)");
+
+
+            
 }
 
 add.addEventListener("click", addTask);
+box.addEventListener("click",valid);
+
+
